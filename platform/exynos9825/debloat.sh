1fed61a5fbb844782b75ae6cfcf95ad6749baf9a
Ocin4ever <winicoco24@gmail.com>
2025-05-01 15:33:49 +0200

    platform: exynos9825: typo

--- platform/exynos9825/debloat.sh
+++ platform/exynos9825/debloat.sh
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Debloat list for the Exynos 9820 platform
+# Debloat list for the Exynos 9825 platform
 # - Add entries inside the specific partition containing that file (<PARTITION>_DEBLOAT+="")
 # - DO NOT add the partition name at the start of any entry (eg. "/system/dpolicy_system")
 # - DO NOT add a slash at the start of any entry (eg. "/dpolicy_system")

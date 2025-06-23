salvogiangri/UN1CA

unica: follow up of 27664c2e
Change-Id: Iaf500441adfba9c5bf61f72be695880c140f8ba1

unica/patches/rro/customize.sh
@@ -1,5 +1,5 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "/product/overlay/framework-res__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__auto_generated_rro_product.apk/res"

Star-Seven/EternityROM

unica/patches/rro/customize.sh
@@ -1,5 +1,5 @@
 if [[ -d "$SRC_DIR/target/$TARGET_CODENAME/overlay" ]]; then
-    DECODE_APK "/product/overlay/framework-res__e2sxxx__auto_generated_rro_product.apk"
+    DECODE_APK "product" "overlay/framework-res__e2sxxx__auto_generated_rro_product.apk"
 
     echo "Applying stock overlay configs"
     rm -rf "$APKTOOL_DIR/product/overlay/framework-res__e2sxxx__auto_generated_rro_product.apk/res"
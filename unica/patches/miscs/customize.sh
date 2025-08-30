Mesazane/TachyonOS

unica: patches: miscs: add some props
Change-Id: I885c36f0f6963312933b90abcdc5671f126d9bc2
Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

unica/patches/miscs/customize.sh
@@ -5,6 +5,9 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
 SET_PROP "system" "ro.build.display.id" "ExtremeROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME ($VALUE)"
 
+SET_PROP "system" "ro.extremerom.version" "$ROM_VERSION"
+SET_PROP "system" "ro.extremerom.codename" "$ROM_CODENAME"
+
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
 SET_PROP "product" "ro.frp.pst" ""

Star-Seven/EternityROM

unica/patches/miscs/customize.sh
@@ -5,6 +5,9 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
 SET_PROP "system" "ro.build.display.id" "EternityROM $ROM_VERSION $ROM_COMMIT ($VALUE)"
 
+SET_PROP "system" "ro.eternityrom.version" "$ROM_VERSION"
+SET_PROP "system" "ro.eternityrom.codename" "$ROM_CODENAME"
+
 # Disable FRP
 SET_PROP "product" "ro.frp.pst" ""
 # N10 doesn't have product partition on stock so this prop was moved to vendor 